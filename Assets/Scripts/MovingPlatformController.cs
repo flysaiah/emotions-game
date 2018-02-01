bf69018bf90e55d759b88f925015942843a68601
flysaiah <mayeis01@yahoo.com>
2018-01-31 23:20:14 -0500

    Added moving platforms

---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour {
+
+    [SerializeField] Vector3 startPoint;
+    [SerializeField] Vector3 endPoint;
+    [SerializeField] float moveSpeed;
+
+    private bool movingForward;
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = startPoint;
+        this.movingForward = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.movingForward) {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, endPoint, moveSpeed * Time.deltaTime);
+        } else {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, startPoint, moveSpeed * Time.deltaTime);
+        }
+
+        if (this.transform.position == endPoint) {
+            this.movingForward = false;
+        } else if (this.transform.position == startPoint) {
+            this.movingForward = true;
+        }
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    [SerializeField] float moveSpeed;
+    [SerializeField] float baseMoveSpeed;
+    [SerializeField] float platformMoveSpeedModifier;
     [SerializeField] float jumpSpeed;
     [SerializeField] float groundCheckRadius;
     [SerializeField] LayerMask whatIsGround;
@@ -12,6 +13,8 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] LevelManager levelManager;
 
     private bool isGrounded;
+    private bool onPlatform;
+    private float moveSpeed;
 
     private SpriteRenderer spriteRenderer;   // Use to change color of character
     private Rigidbody2D rigidBody;
@@ -21,8 +24,9 @@ public class PlayerController : MonoBehaviour {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
         this.rigidBody = GetComponent<Rigidbody2D>();
         this.animator = GetComponent<Animator>();
-
+        this.onPlatform = false;
         this.isGrounded = true;
+        this.moveSpeed = this.baseMoveSpeed;
 	}
 	
 	void Update () {
@@ -32,6 +36,11 @@ public class PlayerController : MonoBehaviour {
 	}
 
     private void moveRightOrLeft() {
+        if (this.onPlatform) {
+            this.moveSpeed = this.baseMoveSpeed * this.platformMoveSpeedModifier;
+        } else {
+            this.moveSpeed = this.baseMoveSpeed;
+        }
         if (Input.GetAxisRaw("Horizontal") > 0) {
             this.transform.localScale = new Vector3(.25f, .25f, 1f);
             this.rigidBody.velocity = new Vector3(moveSpeed, this.rigidBody.velocity.y, 0f);
@@ -55,10 +64,25 @@ public class PlayerController : MonoBehaviour {
         this.animator.SetBool("isGrounded", this.isGrounded);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
+    private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "StreamOfConsciousness") {
             this.levelManager.handlePlayerDeath();
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "MovingPlatform") {
+            this.onPlatform = true;
+            this.transform.parent = collision.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.tag == "MovingPlatform") {
+            this.onPlatform = false;
+            this.transform.parent = null;
+        }
+    }
+
+
 }
